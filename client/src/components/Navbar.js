956576cf2ff8c7bb373e3a4e542d692e2b6da832
Anton Arellano <antonarellano14@gmail.com>
2024-01-27 21:31:55 -0800

    decrease navbar code

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,6 +6,15 @@ import logo from "../images/3.png"; // Replace path_to_logo with the correct pat
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const websiteRoutes = [
+    { path: "/", component: "Home" },
+    { path: "/About", component: "About" },
+    { path: "/Careers", component: "Careers" },
+    { path: "/Staffing", component: "Staffing" },
+    { path: "/MedicalConcierge", component: "Medical Concierge" },
+    { path: "/Contact", component: "Contact" },
+  ];
+
   return (
     <div className="sticky top-0 z-50 mb-5">
       <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 rounded-b-xl">
@@ -54,56 +63,17 @@ export default function Header() {
             </div>
             <div>
               <ul className="font-medium items-center w-full flex flex-col md:flex-row md:items-center">
-                <li>
-                  <a
-                    href="/"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/About"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/Careers"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Careers
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/Staffing"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Staffing
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/MedicalConcierge"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    {" "}
-                    Medical Concierge
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/Contact"
-                    className=" text-2xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Contact Us
-                  </a>
-                </li>
+                {websiteRoutes.map((page) => (
+                  <li key={page.path}>
+                    <a
+                      href={page.path}
+                      className=" text-2xl md:justify-center md:items-center block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      aria-current="page"
+                    >
+                      {page.component}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
